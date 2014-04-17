12568384571b9aabdd9127adbae65afb9a61ed75
NoRest4AWhearry <awhearry@gmail.com>
2014-04-17 17:03:28 -0400

    Created Powerball class with method random_select

--- powerball.rb
+++ powerball.rb
@@ -1,9 +1,17 @@
-white_balls = Array(1..59)
+class Powerball
 
-power_ball = Array(1..35)
+	def initialize
+		@white_ball = Array(1..59)
+		@power_ball = Array(1..35)
+	end
 
-five_num = white_balls.combination(5).to_a.sample(1)
+	def random_select
+		random_five = @white_ball.combination(5).to_a.sample(1)
+		winning_6 = random_five.push(@power_ball[rand(1..35)])
+		p winning_6
+	end
+end
 
-winning_6 = five_num.push(power_ball[rand(1..35)])
+powerball = Powerball.new
 
-p winning_6
+powerball.random_select
